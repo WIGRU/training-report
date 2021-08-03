34450fb7540daf4467a0254630b9cd3a14a6e7d6
William <william.grunder@gmail.com>
2021-08-03 18:42:22 +0200

    new function: summarize months

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ start_date = datetime.strptime(config['DEFAULT']["start_date"], time_format)
 end_date = datetime.strptime(config['DEFAULT']["end_date"], time_format)
 
 activities = [] #dictlist with all activities
-
+all_activities = []
 
 dist_sum = float()
 time_sum = timedelta()
@@ -36,25 +36,49 @@ with open(config['DEFAULT']["in_file"], newline='', encoding='utf-8') as csvfile
                 date_str = row[1].split(" ")[0]
                 date = datetime.strptime(date_str, '%Y-%m-%d')
             if start_date <= date <= end_date or header:
-                activities.append({'aktivitetstyp': row[0], 'datum': row[1].split(" ")[0], 'namn': row[3], 'sträcka': row[4], 'tid': row[6], 'medelpuls': row[7], 'medeltempo': row[12], 'stigning': row[14]})
+                activities.append({'aktivitetstyp': row[0], 'datum': row[1].split(" ")[0], 'namn': row[3], 'sträcka': row[4], 'tid': row[6], 'medelpuls': row[7], 'medeltempo': row[12], 'stigning': row[14]})    
                 if not row[0] == "Aktivitetstyp":
                     dist_sum += float(row[4].replace(",","."))
                     t = datetime.strptime(row[6], "%H:%M:%S")
                     time_sum += timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+            all_activities.append({'aktivitetstyp': row[0], 'datum': row[1].split(" ")[0], 'namn': row[3], 'sträcka': row[4], 'tid': row[6], 'medelpuls': row[7], 'medeltempo': row[12], 'stigning': row[14]})
                     
 print("creating html...")
 activities.reverse()
 activities.insert(0, activities[-1])
 activities.pop(-1)
 
-dist_sum = round(dist_sum, 2)
-hours_sum = round(time_sum.total_seconds() / 3600, 2)
+sum = []
+sum.append({'key': 'Månad', 'dist': 'Distans', 'time': 'Tid', 'count': 'Antal'})
+#Monthly sum
+if config['DEFAULT']['summarize_months'] == 'True':
+    months = set()
+    for activity in all_activities:
+        if activity["datum"] != "Datum":
+            print(activity["datum"])
+            m = datetime.strptime(activity["datum"], time_format)
+            m_str = f"{m.year}-{m.month}"
 
-dist_str = str(dist_sum).replace('.',',')
-hours_str = str(hours_sum).replace('.',',')
+            dist = float(activity['sträcka'].replace(",","."))
+            t = datetime.strptime(activity["tid"], "%H:%M:%S")
+            hours = round(t.hour + t.minute / 60 + t.second / 3600, 2)
+            
+            exists = False
+            for v in sum:
+                if m_str in v.values():
+                    exists = True
+                    new_dist = v['dist'] + dist
+                    new_time = v['time'] + hours
+                    count = v['count'] + 1
+                    sum[sum.index(v)]['dist'] = new_dist
+                    sum[sum.index(v)]['time'] = new_time
+                    sum[sum.index(v)]['count'] = count
+
+            if not exists:
+                sum.append({'key': m_str, 'dist': dist, 'time': hours, 'count': 0})
+
 
 ym = str(start_date.year) + "-" + str(start_date.month)
-
 with open(f'{ config["DEFAULT"]["tmp_folder"] }activities.html', 'w', encoding="utf-8") as file:
     file.write("""
                 <!DOCTYPE html>
@@ -64,7 +88,7 @@ with open(f'{ config["DEFAULT"]["tmp_folder"] }activities.html', 'w', encoding="
                 <style>
                 table {
                     border-collapse: collapse;
-                    width: 100%;
+                    width: auto;
                 }
 
                 th, td {
@@ -85,9 +109,10 @@ with open(f'{ config["DEFAULT"]["tmp_folder"] }activities.html', 'w', encoding="
     file.write(f"<h1>Träningsdagbok, {ym}</h1>")
     name = config["DEFAULT"]["user_name"]
     file.write(f"<b>{ name }</b>")
+    file.write("<h2>Aktiviteter</h2>")
     file.write(tabulate(activities, headers="firstrow", tablefmt="html"))
-    file.write(f"<h3>Distans: {dist_str} km <br></h3>")
-    file.write(f"<h3>Tid: </b> {hours_str} h <h3>")
+    file.write("<h2>Summering</h2>")
+    file.write(tabulate(sum, headers="firstrow", tablefmt="html"))
     file.write("""
                 </body>
                 </html>
